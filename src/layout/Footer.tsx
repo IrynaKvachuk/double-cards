762cb430b590dc58f2a2e88e810a6cec6c0362c0
Iryna Kvachuk <kvachukir@gmail.com>
2023-03-09 17:34:11 +0200

    Footer

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="game-app_footer">
+      <div className="game-app_footer__copyright">&#169; 2023 - all right reserved</div>
+    </footer>
+  );
+};
+
+export default Footer;
