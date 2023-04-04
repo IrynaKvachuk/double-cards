b070700959bc25d0f59b1c711d124de047edfc42
Iryna Kvachuk <kvachukir@gmail.com>
2023-04-04 16:07:08 +0300

    GameState, StopWatch

---
+++ src/pages/DoubleCards/ToolbarTop/GameState.tsx
@@ -0,0 +1,32 @@
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import Stopwatch from '../../../components/StopWatch';
+import {
+  selectGameFinished,
+  selectGameReloaded,
+  selectTurns
+} from '../../../features/DoubleCards/DoubleCardsSelects';
+import { setGameTime } from '../../../features/DoubleCards/DoubleCardsActions';
+
+const GameState: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const gameReloaded = useSelector(selectGameReloaded);
+  const gameFinished = useSelector(selectGameFinished);
+  const doubleCardsTurns = useSelector(selectTurns);
+
+  const saveTimeToStore = (time: number) => {
+    dispatch(setGameTime(time));
+  };
+
+  return (
+    <div className="double-cards_results">
+      <span className="double-cards_results__turns">Turns: {doubleCardsTurns}</span>
+      <span className="double-cards_results__time">
+        <Stopwatch resetTime={gameReloaded} runTimer={!gameFinished} saveTime={saveTimeToStore} />
+      </span>
+    </div>
+  );
+};
+
+export default GameState;
--- src/pages/DoubleCards/ToolbarTop/index.tsx
+++ src/pages/DoubleCards/ToolbarTop/index.tsx
@@ -1,43 +1,18 @@
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import Stopwatch from '../../../components/StopWatch';
-import { setGameTime } from '../../../features/DoubleCards/DoubleCardsActions';
-import {
-  selectGameFinished,
-  selectGameReloaded,
-  selectTurns
-} from '../../../features/DoubleCards/DoubleCardsSelects';
-import ReloadGameBtn from './ReloadGameBtn';
+import GameState from './GameState';
+import GridResize from './actions/GridResize';
+import ReloadGame from './actions/ReloadGame';
 
 type Props = {};
 
 const ToolbarTop: React.FC<Props> = () => {
-  const dispatch = useDispatch();
-
-  const gameReloaded = useSelector(selectGameReloaded);
-  const gameFinished = useSelector(selectGameFinished);
-  const doubleCardsTurns = useSelector(selectTurns);
-
-  const saveTimeToStore = (time: number) => {
-    dispatch(setGameTime(time));
-  };
-
   return (
     <section className="double-cards_toolbar-top">
       <div className="double-cards_toolbar-top__info">
-        <div className="double-cards_results">
-          <span className="double-cards_results__turns">Turns: {doubleCardsTurns}</span>
-          <span className="double-cards_results__time">
-            <Stopwatch
-              resetTime={gameReloaded}
-              runTimer={!gameFinished}
-              saveTime={saveTimeToStore}
-            />
-          </span>
-        </div>
+        <GameState />
       </div>
       <div className="double-cards_toolbar-top__actions">
-        <ReloadGameBtn />
+        <GridResize />
+        <ReloadGame />
       </div>
     </section>
   );
