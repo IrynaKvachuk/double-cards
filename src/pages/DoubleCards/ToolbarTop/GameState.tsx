5d1113e5c0f945a78c6d11959eeb988f24513e41
Iryna Kvachuk <kvachukir@gmail.com>
2023-04-27 20:09:15 +0300

    GameState optimization

--- src/pages/DoubleCards/ToolbarTop/GameState.tsx
+++ src/pages/DoubleCards/ToolbarTop/GameState.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import Stopwatch from '../../../components/StopWatch';
 import {
@@ -9,17 +8,16 @@ import {
 } from '../../../features/DoubleCards/DoubleCardsSelects';
 import { setGameTime } from '../../../features/DoubleCards/DoubleCardsActions';
 import { Timer } from '../../../features/_common/types';
+import store from '../../../store';
+
+const saveTimeToStore = (time: Timer) => {
+  store.dispatch(setGameTime(time));
+};
 
 const GameState: React.FC = React.memo(() => {
-  const dispatch = useDispatch();
-
   const gameReloaded = useSelector(selectGameReloaded);
   const gameFinished = useSelector(selectGameFinished);
   const doubleCardsTurns = useSelector(selectTurns);
-
-  const saveTimeToStore = (time: Timer) => {
-    dispatch(setGameTime(time));
-  };
 
   return (
     <div className="double-cards_results">
