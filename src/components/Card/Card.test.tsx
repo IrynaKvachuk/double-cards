01f5c13ce096c5e5d6c3254405df76a13c833834
Iryna Kvachuk <kvachukir@gmail.com>
2023-04-28 16:27:20 +0300

    Card.test: init

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,10 +1,11 @@
-import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
 import Card from '.';
 import { CardType } from '../../features/Card/CardTypes';
+import store from '../../store';
 
-describe('Card', () => {
-  const { render } = ReactDOM;
-  let container: HTMLDivElement;
+const setUp = () => {
   const card: CardType = {
     // src: `${process.env.PUBLIC_URL}/cards/palm.png`,
     src: `/cards/palm.png`,
@@ -13,19 +14,35 @@ describe('Card', () => {
     matched: false,
     id: 'palm1'
   };
-  const cardProps = {
+
+  const mockData = {
     card,
     disabled: false,
-    openCard: () => {}
+    openCard: jest.fn()
   };
 
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-    // render(<Card {...cardProps} />, container);
-  });
-  afterEach(() => {
-    document.body.removeChild(container);
-    container.remove();
+  const utils = render(
+    <Router>
+      <Provider store={store}>
+        <Card {...mockData} />
+      </Provider>
+    </Router>
+  );
+  const cardContainer = screen.getByTestId('card');
+
+  return {
+    cardContainer,
+    mockData,
+    ...utils
+  };
+};
+describe('Card', () => {
+  beforeEach(() => {});
+  afterEach(() => {});
+
+  test('renders card', () => {
+    const { cardContainer } = setUp();
+
+    expect(cardContainer).toBeInTheDocument();
   });
 });
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -13,17 +13,23 @@ const Card: React.FC<Props> = React.memo((props: Props) => {
   const flippedClass = side === 'front' ? 'flipped' : '';
 
   return (
-    <div className={`double-cards_card ${flippedClass}`} data-disabled={disabled}>
+    <div
+      className={`double-cards_card ${flippedClass}`}
+      data-disabled={disabled}
+      data-testid="card"
+    >
       <img
         src={src}
         className="double-cards_card--front"
         alt="card front"
+        data-testid="card-front"
         onClick={(event) => openedCardClick({ event })}
       />
       <img
         src={`${process.env.PUBLIC_URL}/card-cover.png`}
         className="double-cards_card--back"
         alt="card back"
+        data-testid="card-back"
         onClick={(event) =>
           closedCardClick({
             event,
