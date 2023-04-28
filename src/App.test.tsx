ea34181bbd9bedbf8ae8d7b118577d76ed8eaa84
Iryna Kvachuk <kvachukir@gmail.com>
2023-04-28 16:26:10 +0300

    App.test: provider

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,17 @@
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import store from './store';
 
 test('renders app body', () => {
-  render(<App />);
+  render(
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  );
   const linkElement = screen.getByTestId('app-body');
   expect(linkElement).toBeInTheDocument();
 });
